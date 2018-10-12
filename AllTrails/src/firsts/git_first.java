3c67d6143f409f559dbbe6b56d96332c6ea49a2a
pattulohith <pattulohith@192.168.0.14>
2018-10-11 22:48:42 -0700

    2. added new line in git_first.java to check gitHub push

--- AllTrails/src/firsts/git_first.java
+++ AllTrails/src/firsts/git_first.java
@@ -5,5 +5,6 @@ public class git_first {
 	public static void main(String[] args) {
 		System.out.println("firtst git");
 		System.out.println("new change");
+		System.out.println("changed this line after uploading to github");
 	}
 }
