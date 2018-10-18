0c89c46ad48ccc96eaaac15939773140d4cf5814
AprilRiver <parisoodhana@gmail.com>
2018-10-18 15:52:45 -0700

    ADDED: 1. screenshot taking code 2. Added Reading from XL cose 3. seperated all configuration methods into SetUp.java 4. Added testNG 5. Has: 	* Resuable methods and flows 	* Property files for : config ,object repository and test case data

--- demoproject/testing/scripts/Driver.java
+++ demoproject/testing/scripts/Driver.java
@@ -8,75 +8,15 @@ import org.openqa.selenium.WebElement;
 import com.aventstack.extentreports.ExtentTest;
 
 public class Driver extends RF{
-	static Properties properties = null;
-	static BufferedReader reader;
 
-	static void TC01() throws Exception
-	{
-		
-		//create test case report tab
-		ExtentTest logger = createTestScriptReport("TC01");
-
-		//give data file path
-		readDataFile("./testing/dataFiles/TC01.properties");
-
-		//Resuable flow : opens firefox -> open salesforce login page -> gives input for user name and password 
-		enterUserNamePassword();
-
-		//click login
-		WebElement loginButton = driver.findElement(getLocator("salesforce.login.Button.loginButton"));
-		clickElement(loginButton, "Login button");
-
-		// verify error message is as expected
-		WebElement errorMessage = driver.findElement(getLocator("salesforce.login.errorMessage"));
-		verifyText(errorMessage, "Error message", dataProp.getProperty("expectedErrorMessage"));
-
-		//close browser
-		closeBrowser();
-	}
 	
-	static void TC03() throws Exception
-	{
-		//create test case report tab
-		ExtentTest logger = createTestScriptReport("TC03");
-
-		//give data file path
-		readDataFile("./testing/dataFiles/TC03.properties");
-
-		//Resuable flow : opens firefox -> open salesforce login page -> gives input for user name and password 
-		enterUserNamePassword();
-
-		// check remember me box
-		WebElement rememberMe = driver.findElement(RM.getLocator("salesforce.login.rememberMe"));
-		RM.clickElement(rememberMe, "Remember Me ");
-		
-		//click login
-		WebElement loginButton = driver.findElement(getLocator("salesforce.login.Button.loginButton"));
-		clickElement(loginButton, "Login button");
-	
-		//click user menu arrow
-		WebElement userMenuArrow = driver.findElement(getLocator("salesforce.homePage.userMenu.arrow"));
-		clickElement(userMenuArrow, "User Menu");
-		
-		//click logout
-		WebElement logout = driver.findElement(getLocator("salesforce.homePage.userMenu.Logout"));
-		clickElement(logout, "Logout");
-		
-		Thread.sleep(3000);
-		//verify user name saved or not
-		WebElement savedUserName = driver.findElement(getLocator("salesforce.login.Button.savedUserName"));
-		RM.verifyText(savedUserName, "Saved user name", dataProp.getProperty("userName"));
-
-		//close browser
-		closeBrowser();
-	}
 	public static void main(String[] args) throws Exception {
 		initializeExtentReport();
 
 		readORFile("./testing/Repository/objRep.properties");
 		readConfigFile("./testing/Repository/config.properties");
 		//TC01();
-		TC03();
+		//TC03();
 		System.out.println("from maven");
 		extent.flush();
 
--- demoproject/testing/scripts/RM.java
+++ demoproject/testing/scripts/RM.java
@@ -18,178 +18,16 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-public class RM {
+public class RM extends SetUp {
 
-	static ExtentHtmlReporter htmlReporter = null;
-	static ExtentReports extent = null ;
-	static WebDriver driver = null;
-	static ExtentTest logger = null ;
-	static Properties configProp = null;
-	static Properties dataProp = null ;
-	static Properties OR_Prop = null ;
-
-	public static void initializeExtentReport()
-	{
-		htmlReporter =  new ExtentHtmlReporter("./Reports/exeReport.html");
-		extent= new ExtentReports ();	
-		System.out.println("Created Report");
-	}
-
-	public static  ExtentTest createTestScriptReport(String testCaseName) 
-	{
-		// create a test case  tab and attach to report
-		extent.attachReporter(htmlReporter);
-		logger = extent.createTest(testCaseName);
-		System.out.println("Created test tab");
-		return logger ;
-	}
-
-
-	public static Properties getLoadedPropertiesObject(String FilePath)
-	{
-		Properties propertiesObject;
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(FilePath));
-			propertiesObject = new Properties();
-			try {
-				propertiesObject.load(reader);
-				reader.close();
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		} 
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException("File not found at " + FilePath);
-		}
-		return propertiesObject;
-	}
-
-	//load object with data file contents
-	public static void readDataFile(String dataFilePath)
-	{
-		dataProp = getLoadedPropertiesObject(dataFilePath);
-	}
-
-	//load object with configuration file contents
-	public static void readConfigFile(String configFilePath)
-	{
-		configProp = getLoadedPropertiesObject(configFilePath);
-	}
-
-	//load object with Object Repository file contents
-	public static void readORFile(String ObjRepoFilePath)
-	{
-		OR_Prop = getLoadedPropertiesObject(ObjRepoFilePath);
-	}
-
-	// get locator from object repository file
-	public static By getLocator(String elementKey) throws Exception {
-		String locator = OR_Prop.getProperty(elementKey);
-
-		String locatorType = locator.split(":")[0];
-		String locatorValue = locator.split(":")[1];
-
-		if(locatorType.toLowerCase().equals("id"))
-			return By.id(locatorValue);
-		else if(locatorType.toLowerCase().equals("name"))
-			return By.name(locatorValue);
-		else if((locatorType.toLowerCase().equals("classname")) || (locatorType.toLowerCase().equals("class")))
-			return By.className(locatorValue);
-		else if((locatorType.toLowerCase().equals("tagname")) || (locatorType.toLowerCase().equals("tag")))
-			return By.className(locatorValue);
-		else if((locatorType.toLowerCase().equals("linktext")) || (locatorType.toLowerCase().equals("link")))
-			return By.linkText(locatorValue);
-		else if(locatorType.toLowerCase().equals("partiallinktext"))
-			return By.partialLinkText(locatorValue);
-		else if((locatorType.toLowerCase().equals("cssselector")) || (locatorType.toLowerCase().equals("css")))
-			return By.cssSelector(locatorValue);
-		else if(locatorType.toLowerCase().equals("xpath"))
-			return By.xpath(locatorValue);
-		else
-			throw new Exception("Unknown locator type '" + locatorType + "'");
-	}
-
-	// gets driver path -> from config.properties
-	public static String getDriverPath(String webBrowserName)
-	{
-
-		String propertyFilePath= "./testing/Repository/config.properties";
-		BufferedReader reader;
-
-		try {
-			reader = new BufferedReader(new FileReader(propertyFilePath));
-			configProp = new Properties();
-			try {
-				configProp.load(reader);
-				reader.close();
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		} 
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
-		}	
-
-		if (webBrowserName.equalsIgnoreCase("firefox"))
-		{
-			String geckoPath = configProp.getProperty("firefox_driver_path");
-			if(geckoPath!= null) 
-				return geckoPath;
-			else 
-			{
-				//throw new RuntimeException("firefox driverPath is not specified in the Configuration.properties file.");
-				return "cant find driver ";
-			}
-		}
-
-		else if (webBrowserName.equalsIgnoreCase("chrome"))
-		{
-			System.out.println("chrome pending");
-			return "cant find driver ";
-
-		}
-		else if (webBrowserName.equalsIgnoreCase("IE"))
-		{
-			System.out.println("IE pending");
-			return "cant find driver ";
-		}
-		else return "cant find driver ";
-	}
-
-	// open browser
-	public static void  openBrowser(String webBrowserName) 
-	{	
-		if (webBrowserName.equalsIgnoreCase("firefox"))
-		{
-			System.setProperty("webdriver.gecko.driver",getDriverPath(webBrowserName));
-			System.out.println(getDriverPath(webBrowserName));
-			driver=new FirefoxDriver();
-
-			//get implicit wait from config file
-			int waitTime = Integer.parseInt(configProp.getProperty("implicit_wait_time"));
-
-			driver.manage().timeouts().implicitlyWait(waitTime, TimeUnit.SECONDS);
-			logger.log(Status.PASS, MarkupHelper.createLabel("Opened : Firefox", ExtentColor.GREEN));
-			System.out.println("Opened : Firefox");
-		}
-		else if (webBrowserName.equalsIgnoreCase("chrome"))
-		{
-			logger.log(Status.FAIL, MarkupHelper.createLabel("chrome NOT ready", ExtentColor.RED));
-			System.out.println("CHROME not ready yet ");
-		}
-		else if (webBrowserName.equalsIgnoreCase("IE"))
-		{
-			logger.log(Status.FAIL, MarkupHelper.createLabel("IE NOT ready", ExtentColor.RED));
-			System.out.println("IE not ready yet ");
-		}
-
-	}
-
+	/* 
+	 * Name of the Method: enterText
+	 * Brief Description: Enters text to text field
+	 * Arguments: obj --> Web object, textVal --> Text value to be entered in text box, objName --> Nam eof the object
+	 * Created By: Automation team
+	 * Creation Date: Oct 09 2018
+	 * Last Modified: Oct 09 2018 
+	 */
 	// open web page -> URL : data file of TC
 	public static void  openWebPage(String pageName)
 	{
@@ -198,10 +36,17 @@ public class RM {
 		logger.log(Status.PASS, MarkupHelper.createLabel("Opened :  "+pageName, ExtentColor.GREEN));
 		System.out.println(URL +" openened");
 	}
-	/*Name :
-	 *
-	 * *
+
+
+	/* 
+	 * Name of the Method: enterText
+	 * Brief Description: Enters text to text field
+	 * Arguments: obj --> Web object, textVal --> Text value to be entered in text box, objName --> Nam eof the object
+	 * Created By: Automation team
+	 * Creation Date: Oct 09 2018
+	 * Last Modified: Oct 09 2018 
 	 */
+
 	public static void sendText(WebElement webElement , String webElementName , String text)
 	{
 		if(webElement.isDisplayed())
@@ -218,10 +63,17 @@ public class RM {
 		}
 	}
 
-	/*
-	 * 
-	 * 
-	 * */
+
+
+	/* 
+	 * Name of the Method: enterText
+	 * Brief Description: Enters text to text field
+	 * Arguments: obj --> Web object, textVal --> Text value to be entered in text box, objName --> Nam eof the object
+	 * Created By: Automation team
+	 * Creation Date: Oct 09 2018
+	 * Last Modified: Oct 09 2018 
+	 */
+
 	public static void clickElement(WebElement webElement , String webElementName )
 	{
 		if(webElement.isDisplayed())
@@ -236,26 +88,33 @@ public class RM {
 			logger.log(Status.FAIL, MarkupHelper.createLabel("Can't locate :  "+webElementName, ExtentColor.RED));
 		}
 	}
-	/*
-	 * 
-	 * */
-	/*Name :
-	 *
-	 * *
+
+
+	/* 
+	 * Name of the Method: enterText
+	 * Brief Description: Enters text to text field
+	 * Arguments: obj --> Web object, textVal --> Text value to be entered in text box, objName --> Nam eof the object
+	 * Created By: Automation team
+	 * Creation Date: Oct 09 2018
+	 * Last Modified: Oct 09 2018 
 	 */
-	public static void verifyText(WebElement webElement , String webElementName , String expectedText)
+
+	public static void verifyText(WebElement webElement , String webElementName , String expectedText) throws IOException
 	{
 		if(webElement.isDisplayed())
 		{
 			if(webElement.getText().equals(expectedText))
 			{
 				logger.log(Status.PASS, MarkupHelper.createLabel("As expected : "+webElementName+" = '"+webElement.getText()+" '", ExtentColor.GREEN));
-				System.out.println("Deviation : '"+webElement.getText()+"' displayed instead of  '" +expectedText+ "'");
+				System.out.println("As expected : "+webElementName+" = '"+webElement.getText()+" '");
 			}	
 			else
 			{
 				logger.log(Status.FAIL, MarkupHelper.createLabel("Deviation : '"+webElement.getText()+"' displayed instead of  '" +expectedText+ "'", ExtentColor.RED));
 				System.out.println("Deviation : '"+webElement.getText()+"' displayed instead of  '" +expectedText+ "'");
+				
+				String imagePath=takeScreenShot();
+				logger.addScreenCaptureFromPath(imagePath);
 			}
 		}
 		else
@@ -265,6 +124,15 @@ public class RM {
 		}
 	}
 
+
+	/* 
+	 * Name of the Method: enterText
+	 * Brief Description: Enters text to text field
+	 * Arguments: obj --> Web object, textVal --> Text value to be entered in text box, objName --> Nam eof the object
+	 * Created By: Automation team
+	 * Creation Date: Oct 09 2018
+	 * Last Modified: Oct 09 2018 
+	 */
 
 	public static void closeBrowser ()
 	{
